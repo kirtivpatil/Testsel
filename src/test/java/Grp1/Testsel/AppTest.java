afe2cac12629ef3dc1f1656182c64e0241e61b34
kirtivpatil <kirtivpatil@gmail.com>
2022-05-29 14:01:21 -0400

    first commit

---
+++ src/test/java/Grp1/Testsel/AppTest.java
@@ -0,0 +1,20 @@
+package Grp1.Testsel;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
